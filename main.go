540da409c98e8e4578231ec81954b608dd45fcdd
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2023-04-09 13:45:27 -0700

    Enabling CORS

--- main.go
+++ main.go
@@ -27,16 +27,12 @@ func messageHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Convert the drinks list to a prompt for the GPT message.
-	drinksList := strings.Join(payload.Drinks, ", ")
 	inputMessageTemplate, err := gcp.ReadFile("cocktails-ai", "gpt4-prompt.txt")
 	if err != nil {
 		fmt.Sprintf("Error calling ReadFile function: %v", err)
 		return
 	}
-	inputMessage := fmt.Sprintf(inputMessageTemplate, drinksList)
-
-	response, err := gpt.Message(inputMessage)
+	response, err := requestGpt(payload.Drinks, inputMessageTemplate)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error calling Message function: %v", err), http.StatusInternalServerError)
 		return
@@ -46,8 +42,34 @@ func messageHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		// If it's an OPTIONS request, respond with just the headers, otherwise call the next handler.
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func requestGpt(drinks []string, promptTemplate string) (gpt.GptChatCompletionMessage, error) {
+	drinksList := strings.Join(drinks, ", ")
+	inputMessage := fmt.Sprintf(promptTemplate, drinksList)
+
+	response, err := gpt.Message(inputMessage)
+	if err != nil {
+		return gpt.GptChatCompletionMessage{}, err
+	}
+	return response, nil
+}
+
 func main() {
-	http.HandleFunc("/cocktails", messageHandler)
+	http.Handle("/cocktails", corsMiddleware(http.HandlerFunc(messageHandler)))
 
 	fmt.Println("Starting server at :8080")
 	http.ListenAndServe(":8080", nil)
@@ -55,7 +77,14 @@ func main() {
 
 // func main() {
 // 	drinksList := strings.Join([]string{"vodka", "lime juice", "gyn"}, ", ")
-// 	inputMessageTemplate, err := gcp.ReadFile("cocktails-ai", "gpt4-prompt.txt")
+// 	// inputMessageTemplate, err := gcp.ReadFile("cocktails-ai", "gpt4-prompt.txt")
+
+// 	inputMessageTemplateBuffer, err := ioutil.ReadFile("./prompt.txt")
+// 	if err != nil {
+// 		log.Fatalf("Error reading file: %v", err)
+// 	}
+
+// 	inputMessageTemplate := string(inputMessageTemplateBuffer)
 // 	if err != nil {
 // 		fmt.Sprintf("Error calling ReadFile function: %v", err)
 // 		return
---
+++ test-remote.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST -H "Content-Type: application/json" -d '{"drinks": ["vodka", "gin", "tequila", "rum", "triple sec", "lemon juice"]}' https://cocktails-ewguxkvnaa-uc.a.run.app/cocktails
